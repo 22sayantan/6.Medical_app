cb6b4eadf8742a55f149e542ea90ab86ac4f43fb
sayantandolui97@gmail.com <sayantandolui97@gmail.com>
2023-08-18 03:00:28 +0530

    about add

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 import Home from '../src/components/pages/Home/Home';
-
+import About from "./components/pages/About/About";
 import Cart from "./components/pages/Cart/Cart";
 import UserProfile from "./components/pages/UserProfile/UserProfile";
 import Medicine from "./components/pages/Medicine/Medicine";
@@ -13,7 +13,9 @@ function App() {
   return (
     <>
        <Routes>
+        
         <Route index path='/' element={<Home/>}/>
+        <Route exact path='/cart' element={<About/>}/>
         <Route exact path='/cart' element={<Cart/>}/>
         <Route exact path='/signin' element={<UserProfile/>}/>
       </Routes>
